f2a9400d85e32499831063131aec7f8b1bb32c93
StokkiLife <StockMaster246@gmail.com>
2025-10-07 22:25:52 -0300

    Continuando com o projeto

--- StokkiLife/resources/views/login.blade.php
+++ StokkiLife/resources/views/login.blade.php
@@ -6,7 +6,7 @@
     <title>Login - StokkiLife</title>
     <link rel="icon" href="{{ asset('imgs/stokkilife-logo.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ url('stylesheet/cadastro-login.css') }}">
-    <link rel="stylesheet" href="{{ url('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.css">
 </head>
 <body>
     <article>
@@ -15,7 +15,6 @@
         </div>
         <form method="POST">
             <div class="formulario-login">
-                {{-- Ícones de framework front-end adicionados em login --}}
                 <p><i class="bi bi-envelope-fill"></i> <label for="email">Email: </label>
                     <input type="email" name="email" required>
                 </p>
@@ -25,7 +24,6 @@
                     <input type="password" name="senha" required>
                 </p>
             </div>
-            {{-- adicionando botão de submit para envio de dados em Login --}}
             <input type="submit" value="ENTRAR">
         </form>
     </article>
--- StokkiLife/resources/views/stokkilifeLogin.blade.php
+++ StokkiLife/resources/views/stokkilifeLogin.blade.php
@@ -14,8 +14,6 @@
         <div class="logo">
             <img src="{{ asset('imgs/stokkilife-logo.png') }}" alt="stokkilife logo">
         </div>
-        {{-- a tag a só é utilizado para inserir links de navegação e não para envio de um formulário --}}
-        {{-- a tag form colocado antes estava incorretamente utilizada --}}
         <input type="button" onclick="location.href='{{ url('login') }}'" value="LOGIN">
         </div>
     </article>
